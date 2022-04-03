45ca86fece87a7cfcc1dc463bf0454139153e4ac
Nina Lisitsinskaya <vvatashi@protonmail.com>
2022-04-03 09:44:46 +0600

    Add icons in sidebar

--- src/templates/sidebar.ts
+++ src/templates/sidebar.ts
@@ -1,4 +1,5 @@
 import { html } from '@popeindustries/lit-html-server';
+import config from '../config';
 import Board from '../models/board';
 
 interface SidebarProps {
@@ -10,29 +11,28 @@ interface SidebarProps {
 export function sidebar({ className, path, boards }: SidebarProps) {
   className = [className, 'sidebar'].filter((c) => c).join(' ');
 
-  return html`<aside class="${className}">
+  return html`<aside class=${className}>
     <nav class="sidebar__inner">
-      <ul class="sidebar__list">
-        <li class="sidebar__item">
-          <a
-            class="${['sidebar__link', path === '/' ? 'sidebar__link_active' : null].filter((c) => c).join(' ')}"
-            href="/"
-            >Главная</a
-          >
-        </li>
+      <a class="sidebar__title" href="/">
+        <span class="icon icon_logo"></span>
+        ${config.site.title}
+      </a>
 
+      <ul class="sidebar__list">
         ${boards.slice(0, 5).map(
           (board) => html`<li class="sidebar__item">
             <a
-              class="${[
+              class=${[
                 'sidebar__link',
                 path === `/${board.slug}` || path.startsWith(`/${board.slug}/`) ? 'sidebar__link_active' : null,
               ]
                 .filter((c) => c)
-                .join(' ')}"
-              href="${`/${board.slug}/`}"
-              >${board.title}</a
+                .join(' ')}
+              href=${`/${board.slug}/`}
             >
+              <span class="icon icon_discussion-mask"></span>
+              ${board.title}
+            </a>
           </li>`
         )}
       </ul>
